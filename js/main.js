9ec2c3b1d1f8e39580c2e9bdf58b849cdf76acfa
Ilbert Roque <ilbertj.roque@gmail.com>
2019-03-30 19:42:37 -0400

    Improved Mobile integration

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Calculate BAC
+
 function bacCalc() {
 	var genderCoe;
 	var gender = document.getElementById('gender').value;
@@ -18,6 +20,8 @@ function bacCalc() {
 		genderCoe = 0.55;
 	}
 
+	//Equations
+
 	alcoholDose = (beers + wine + cocktails + spirits) * 14;
 	bodyCalc = weight * 454 * genderCoe;
 	alcoholPerGram = alcoholDose / bodyCalc * 100;
@@ -26,3 +30,49 @@ function bacCalc() {
 
 	document.getElementById('result').innerHTML = result.toFixed(3);
 }
+
+//Change nav bar on scroll
+
+let scrollpos = window.scrollY;
+const header = document.querySelector('#navbar');
+const header_height = header.offsetHeight;
+const add_class_on_scroll = () => header.classList.add('nav-alt');
+const remove_class_on_scroll = () => header.classList.remove('nav-alt');
+
+window.addEventListener('scroll', function() {
+	scrollpos = window.scrollY;
+	if (scrollpos >= header_height) {
+		add_class_on_scroll();
+	} else {
+		remove_class_on_scroll();
+	}
+	console.log(scrollpos);
+});
+
+window.smoothScroll = function(target) {
+	var scrollContainer = target;
+	do {
+		//find scroll container
+		scrollContainer = scrollContainer.parentNode;
+		if (!scrollContainer) return;
+		scrollContainer.scrollTop += 1;
+	} while (scrollContainer.scrollTop == 0);
+
+	var targetY = 0;
+	do {
+		//find the top of target relatively to the container
+		if (target == scrollContainer) break;
+		targetY += target.offsetTop;
+	} while ((target = target.offsetParent));
+
+	scroll = function(c, a, b, i) {
+		i++;
+		if (i > 30) return;
+		c.scrollTop = a + (b - a) / 30 * i;
+		setTimeout(function() {
+			scroll(c, a, b, i);
+		}, 10);
+	};
+	// start scrolling
+	scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+};
