6603dd1b1267ed9528b662fccc4a4b016dc743dc
Ilbert Roque <ilbertj.roque@gmail.com>
2019-04-14 14:47:51 -0400

    Updates

---
+++ precache-manifest.ffa8e639bd05e4662b7a03c4171e7e70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c8904730526733020e6",
+    "url": "/BAC-Calculator/static/css/main.c6a4bf2e.chunk.css"
+  },
+  {
+    "revision": "3c8904730526733020e6",
+    "url": "/BAC-Calculator/static/js/main.06ef544d.chunk.js"
+  },
+  {
+    "revision": "1e0d0cd9f976a124f513",
+    "url": "/BAC-Calculator/static/js/runtime~main.cc4b2c29.js"
+  },
+  {
+    "revision": "3c5835cda3eaeace52b8",
+    "url": "/BAC-Calculator/static/css/2.0828abd8.chunk.css"
+  },
+  {
+    "revision": "3c5835cda3eaeace52b8",
+    "url": "/BAC-Calculator/static/js/2.b0d5ea60.chunk.js"
+  },
+  {
+    "revision": "b5cd2c5c8b694e3abf261880ab55aef7",
+    "url": "/BAC-Calculator/index.html"
+  }
+];
